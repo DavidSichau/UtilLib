0c9c3865461c1a244c0584693687ae1e76e793e7
Tanaka Simon <tanakas@gmx.ch>
2013-06-24 09:26:39 +0200

    added QuadTree test -but fails.

--- test/geometry/QuadTree_test.cpp
+++ test/geometry/QuadTree_test.cpp
@@ -32,6 +32,7 @@ using namespace boost::unit_test;
 using namespace UtilLib;
 
 
+//need a simple node class to test QuadTree:
 class SimpleNode {
  public:
     SimpleNode(
@@ -66,49 +67,6 @@ private:
     double YPos_;
     const unsigned int id_;
 };
-
-
-void test_Constructor() {
-    geometry::Rectangle rect(1.0, 2.0, 3.0, 4.0);
-    BOOST_CHECK(rect.north == 1.0);
-    BOOST_CHECK(rect.south == 2.0);
-    BOOST_CHECK(rect.west == 3.0);
-    BOOST_CHECK(rect.east == 4.0);
-
-    geometry::Rectangle rect2(4.0, 4.0, 4.0);
-    BOOST_CHECK(rect2.north == 6.0);
-    BOOST_CHECK(rect2.south == 2.0);
-    BOOST_CHECK(rect2.west == 2.0);
-    BOOST_CHECK(rect2.east == 6.0);
-
-    geometry::Rectangle rect3(rect);
-    BOOST_CHECK(rect3.north == 1.0);
-    BOOST_CHECK(rect3.south == 2.0);
-    BOOST_CHECK(rect3.west == 3.0);
-    BOOST_CHECK(rect3.east == 4.0);
-}
-
-void test_withhin() {
-    geometry::Rectangle rect2(4.0, 4.0, 4.0);
-    geometry::Rectangle rect3(4.0, 4.0, 2.0);
-    geometry::Rectangle rect4(4.0, 4.0, 6.0);
-
-    BOOST_CHECK(rect2.within(rect3) == true);
-    BOOST_CHECK(rect2.within(rect4) == false);
-
-    geometry::Rectangle rect(4.0, 2.0, 2.0, 4.0);
-    BOOST_CHECK(rect.within(3.5, 2.5, 2.5, 5.0) == false);
-    BOOST_CHECK(rect.within(4.5, 2.5, 2.5, 3.5) == false);
-    BOOST_CHECK(rect.within(3.5, 1.5, 2.5, 3.0) == false);
-    BOOST_CHECK(rect.within(3.5, 2.5, 1.5, 3.0) == false);
-    BOOST_CHECK(rect.within(3.5, 2.5, 2.5, 3.0) == true);
-}
-
-void test_pointWithinBounds() {
-    geometry::Rectangle rect(4.0, 2.0, 2.0, 4.0);
-    BOOST_CHECK(rect.pointWithinBounds(3.0, 3.0) == true);
-    BOOST_CHECK(rect.pointWithinBounds(5.0, 3.0) == false);
-}
 
 void test_get1(void) {
     double x = 1.1;
